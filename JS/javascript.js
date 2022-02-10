6b71bad8f7957de6001846549c66be190133960b
cthomas265 <98835210+cthomas265@users.noreply.github.com>
2022-02-09 19:25:42 -0600

    Prework

---
+++ JS/javascript.js
@@ -0,0 +1,10 @@
+
+
+    document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height = "175px";
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("box").color:"blue";
+});
+
